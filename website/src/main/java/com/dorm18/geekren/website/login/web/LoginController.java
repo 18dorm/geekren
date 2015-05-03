c45f39208c7368b7f098e6e9f971fa1c3ac5551a
wu.xiaomin <wuxiaomin@wuxiaomindeMacBook-Pro.local>
2015-05-03 12:15:05 +0800

    增加了springmvc框架。实现了restfulapi监听/api下的url

---
+++ website/src/main/java/com/dorm18/geekren/website/login/web/LoginController.java
@@ -0,0 +1,15 @@
+package com.dorm18.geekren.website.login.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	@RequestMapping("/api/login")
+	public User login(String username, String pwd) {
+		User ret = new User();
+		ret.setEmail("rosicky1985@163.com");
+		ret.setPassword("123");
+		return ret;
+	}
+}
