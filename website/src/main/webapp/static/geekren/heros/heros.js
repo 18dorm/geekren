dd6fd5cec4578ff707fcd73487a2441cf59d3964
peter future <peter_future@outlook.com>
2015-05-29 12:52:16 +0800

    grab repo & event info

--- website/src/main/webapp/static/geekren/heros/heros.js
+++ website/src/main/webapp/static/geekren/heros/heros.js
@@ -31,9 +31,32 @@ var search_controller = geekren_heros.controller('heros_search', ['$scope', '$ro
 			$http.get(url)
 			.success(function(data){
 				$scope.githuber = data;
+				get_repo_info($scope.githuber.repos_url);
 				// display user info
 				$scope.search_style="";
 			});
 		}
+
+		// get repo info
+		var get_repo_info = function(url){
+			$http.get(url)
+			.success(function(data){
+				$scope.githuber.repos = data;
+				var event_url = $scope.githuber.events_url;
+				get_events(event_url.substr(0, event_url.length - 10));
+			});
+		}
+
+		// get events
+		var get_events = function(url){
+			$http.get(url)
+			.success(function(data){
+				if(data.length > 5)
+					data.length=5;
+				$scope.githuber.events = data;
+				//alert($scope.githuber.events[0].type);
+			});
+		}
+
 		//--------------------------------
 	}]);
