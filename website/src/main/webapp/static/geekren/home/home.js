115589cb26ab76073c915e283d5b5922a529b070
peter-s <peter_future@outlook.com>
2015-06-04 20:52:41 +0800

    jump to user details page

--- website/src/main/webapp/static/geekren/home/home.js
+++ website/src/main/webapp/static/geekren/home/home.js
@@ -8,3 +8,29 @@ geekren.config(function($routeProvider){
                 templateUrl : 'static/geekren/home/home.html',
             })
 });
+
+var search_controller = geekren.controller('home_controller', ['$scope', '$routeParams', '$http',function ($scope, $routeParams, $http) {
+		//--------------------------------
+		$scope.githuber ={};
+		$scope.search_style="display:none";
+		$scope.loading_style="display:none";
+		$scope.search_content = "";
+		$scope.github_search=function(){
+			$scope.loading_style="";
+			$scope.search_style="display:none";
+			var url = "https://api.github.com/users/" + $scope.search_content;
+			$http.get(url)
+			.success(function(data){
+				$scope.githuber = data;
+				get_repo_info($scope.githuber.repos_url);
+			});
+		}
+
+		// get repo info
+		var show_details = function(url){
+			alert("hello");
+			window.location.href="#heros";
+		}
+
+		//--------------------------------
+	}]);
